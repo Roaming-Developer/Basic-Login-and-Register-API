2cb981d61e30b3b64048c890e3cc3a8451a1f60e
Akash Prasher <prince.akash.prasher@gmail.com>
2022-01-18 20:34:35 +0530

    Adding verifyToken for JWT token
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,21 +2,22 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 module.exports = {
-  verifyToken: (req, res, next) => {
-    console.log(req);
-    const token =
-      req.body.token || req.query.token || req.header["x-access-token"];
-
+  verifyToken: async (req, res, next) => {
+    // console.log(req.headers);
+    const token = req.headers.authorization;
+    // console.log(token);
     if (!token) {
       return res.status(403).json({ warning: "Token is required!" });
     }
 
     try {
-      const decode = jwt.verify(token, config.TOKEN_KEY);
-      req.user = decode;
+      let payload = await jwt.verify(token, config.TOKEN_KEY);
+      if (payload) {
+        req.user = payload;
+        return next();
+      }
     } catch (error) {
       return res.status(401).send("Invalid Token");
     }
-    return next();
   },
 };
